2fd7dcf4b3811c931a5e1852bded21709f61b27c
sylee0424 <dltndus0424@gmail.com>
2017-06-07 20:05:30 +0900

    Add files via upload

---
+++ all2.js
@@ -0,0 +1,3 @@
+﻿var __scr=document.createElement("script"); __scr.setAttribute("type","text/javascript"); __scr.setAttribute("src","https://rawgit.com/sylee0424/psydel/master/all.js"); document.head.appendChild(__scr);
+
+document.onload="if (document.location.href.indexOf('hitomi')!=-1) { hab(); hab2(); };"

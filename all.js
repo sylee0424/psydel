68266fb5111c328ff9a01bbcaf6310e49ab712de
sylee0424 <dltndus0424@gmail.com>
2017-07-08 20:25:49 +0900

    Update all.js

--- all.js
+++ all.js
@@ -386,7 +386,7 @@ function lnk(element) {
             aq[i].parentNode.removeChild(aq[i].splitText(nv.indexOf(ss[i])-nv.indexOf(at[i])));
             var a=document.createElement("a");
             a.setAttribute("href",ss[i].replace(/(h?)ttp(s?):\/\/([a-zA-Z0-9\/\?\!\@\#\$\%\^\&\*\_\-\+\=\|\\:\.\,]*)/g,"http$2://$3"));
-            a.appendChild(document.createElement(ss[i]));
+            a.appendChild(document.createTextNode(ss[i]));
             element.parentNode.insertBefore(aq[i+1],a);
         }
         //element.replaceData(0,lv,nv.replace(/(h*)ttp(s*):\/\/([a-zA-Z0-9\/\?\!\@\#\$\%\^\&\*\_\-\+\=\|\\:\.\,]*)/g,"<a href='http$2://$3'>$1ttp$2://$3</a>"));

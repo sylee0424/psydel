35a9c5e0143e525d7f49d738916a46c3d9936854
sylee0424 <dltndus0424@gmail.com>
2018-05-07 14:21:14 +0900

    Add files via upload

--- pop/content.js
+++ pop/content.js
@@ -17,6 +17,28 @@ function addscript(scriptlist,removenode) {
 
 var extension=(!!chrome)?chrome:browser;
 
-window.addEventListener("load", function () {
-	addscript(["pop/dataurls.js","pop/pageobject.js","pop/pagescript.js","pop/modal/modal.js"],true);
-});
+function getChromeVersion() {
+    var pieces = navigator.userAgent.match(/Chrom(?:e|ium)\/([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)/);
+    if (pieces == null || pieces.length != 5) {
+        return undefined;
+    }
+    pieces = pieces.map(piece => parseInt(piece, 10));
+    return {
+        major: pieces[1],
+        minor: pieces[2],
+        build: pieces[3],
+        patch: pieces[4]
+    };
+}
+
+if (getChromeVersion().major>=55) {
+	window.addEventListener("load", function () {
+		addscript(["pop/dataurls.js","pop/pageobject.js","pop/pagescript.js","pop/modal/modal.js"],true);
+	});
+}
+else {
+	window.addEventListener("load", function () {
+		addscript(["pop/dataurls.js","pop/pageobject_alt.js","pop/pagescript.js","pop/modal/modal.js"],true);
+	});
+}
+
